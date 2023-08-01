7f540627ba1e81df0007e361b4822c3647b0ae85
jeff-wamugi <78215278+jeff-wamugi@users.noreply.github.com>
2023-08-01 07:22:29 +0300

    Completed Tasks

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" 2-app mod """
+from flask import Flask, request, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+def get_locale() -> str:
+    """ get the translation best match """
+    return request.accept_languages.best_match(app.config.LANGUAGES)
+
+
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/', strict_slashes=False)
+def hello() -> str:
+    """ simple template """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
